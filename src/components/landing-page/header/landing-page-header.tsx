f0cfc2d6df0267a7249edf10136d33b91f7ae50d
Dominik <samseldominik@gmail.com>
2020-04-19 22:24:58 +0200

    Added text to landing page

--- src/components/landing-page/header/landing-page-header.tsx
+++ src/components/landing-page/header/landing-page-header.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './landing-page-header.scss'
-import Img from 'gatsby-image';
 import logo from "./../../images/logo.png";
 
 function LandingPageHeader(){
     return (
-        <header>
-            <img src={logo} alt="Logo" />
-            <h3>Memos</h3>
-        </header>   
+        <div className="header-container">
+            <div className="max-width header-flex">
+                <div className="header-logo">
+                    <img className="logo" src={logo} alt="Logo" />
+                    <h3 className="header-name">Memos</h3>
+                </div>
+                <ul className="header-links">
+                    <li>Manual</li>
+                    <li className="highlight-text">GAME</li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextLoop from "react-text-loop";
 import './landing-page.scss'
 import LandingPageHeader from "./header";
 
 function LandingPage(){
+    const [cards, setCards] = useState(0);
+    const [name, setName] = useState("");
+
     return (
-        <LandingPageHeader />
+        <div>
+            <LandingPageHeader />
+            <h1 className="highlight-text">Memos</h1>
+            <h2>
+                Do you have
+                {" "}
+                <TextLoop
+                springConfig={{ stiffness: 70, damping: 31 }}
+                adjustingSpeed={500}
+                >
+                    <span className="semihighlight-text">eagle eye</span>
+                    <span className="semihighlight-text">computer memory</span>
+                    <span className="semihighlight-text">sharp mind</span>
+                </TextLoop>
+                ? 
+                <div>
+                You can check it right here.
+                </div>
+            </h2>
+        </div>
     );
 }
 
